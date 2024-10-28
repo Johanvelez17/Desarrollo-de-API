b1ce745f8cef16cd6412cd1bc1ed94d0f6d421ed
danielrun <164501868+danielrun@users.noreply.github.com>
2024-10-28 12:26:27 -0500

    Se crean nuevos datos en JS

--- src/controllers/controladorenfermedad.js
+++ src/controllers/controladorenfermedad.js
@@ -1,5 +1,5 @@
-let nombreEnfermedad=document.getElementById("nombreenfermedad")
-let sintomasEnfermedad=document.getElementById("sintomasenfermedad")
-let clasificacionEnfermedad=document.getElementById("clasificacionenfermedad")
-let gradoEnfermedad=document.getElementById("gradoenfermedad")
-let probabilidadDeVidaEnfermedad=document.getElementById("probabilidadvidaenfermedad")
+let nombreEnfermedad = document.getElementById("nombreenfermedad");
+let sintomasEnfermedad = document.getElementById("sintomasenfermedad");
+let clasificacionEnfermedad = document.getElementById("clasificacionenfermedad");
+let gradoEnfermedad = document.getElementById("gradoenfermedad");
+let probabilidadDeVidaEnfermedad = document.getElementById("probabilidadvidaenfermedad");
--- src/controllers/controladorsignovital.js
+++ src/controllers/controladorsignovital.js
@@ -1,3 +1,27 @@
-let nombreSignovital=document.getElementById("nombresignovital")
-let valorSignovital=document.getElementById("valorsignovital")
-let fechaMedidaSignovital=document.getElementById("fechamedidasignovital")
+let nombreSignoVital = document.getElementById("nombresignovital");
+let valorSignoVital = document.getElementById("valorsignovital");
+let fechaMedidaSignoVital = document.getElementById("fechamedidasignovital");
+
+
+let botonRegistroSignoVital = document.getElementById("botonregistroSignoVital");
+ 
+ botonregistroSignoVital.addEventListener("click", function(evento){
+    evento.preventDefault();
+    // 5. Se crea un JSON que capture todos los datos del formulario
+    
+    let datosFormularioSignoVital = {
+      nombre: nombreSignoVital.value,
+      valor: valorSignoVital.value,
+      fechaMedida: fechaMedidaSignoVital.value
+    };
+    
+    // 6. Se envían los datos al back
+    
+    console.log(datosFormularioSignoVital)
+    
+    Swal.fire({
+      title: "Registro Exitoso",
+      text: "Ya eres parte de nuestra gran familia!",
+      icon: "success"
+    });
+})
